3e0c64e7e28f180cbaf0cd5685071c87669c0e82
Kyle Chun <kbchun5712@gmail.com>
2016-09-30 12:30:56 -0700

    Complete react-components

--- app.js
+++ app.js
@@ -1,1 +1,66 @@
 // TODO
+var GroceryList = (props) => (
+  <ul>
+    {
+      props.groceryItem.map(groceryItem => 
+        <GroceryListItem key={groceryItem} groceryItem={groceryItem} />
+    )}
+  </ul>
+);
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: false,
+      bolded: false
+    };
+  }
+
+  onMouseEnter(event) {
+    this.setState({
+      bolded: !this.state.bolded
+    });
+  }
+
+  onMouseLeave(event) {
+    this.setState({
+      bolded: !this.state.bolded
+    });
+  }
+
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none',
+      fontWeight: this.state.bolded ? 'bold' : 'normal'
+    };
+    return (
+      <li style={style} 
+      onMouseEnter={this.onMouseEnter.bind(this)}
+      onMouseLeave={this.onMouseLeave.bind(this)}
+      onClick={this.onListItemClick.bind(this)}
+      >
+      {this.props.groceryItem}
+      </li>
+    );
+  }
+}
+
+
+var groceryList = ['Apple', 'Orange', 'Banana'];
+
+var App = () => (
+  <div>
+    <h2>Grocery List</h2>
+    <GroceryList groceryItem={groceryList}/>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
